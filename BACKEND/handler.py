3bac64b14eddfff23fc41be5b30764028f7758ec
Tanush Golwala <tanushgolwala@gmail.com>
2024-04-13 21:53:08 +0530

    fix : print

--- BACKEND/handler.py
+++ BACKEND/handler.py
@@ -8,35 +8,39 @@ import deepl
 deepl_api_key = os.getenv('DEEPL_API_KEY')
 translator = deepl.Translator('16f7a326-13d2-42df-b379-3c6b56c0acd7:fx')
 
+
 def translate(text, source, target):
-    sources = ["BG", "CS", "DA", "DE", "EL", "EN", "ES", "ET", "FI", "FR", "HU", "ID", "IT", "JA", "KO", "LT", "LV", "NB", "NL", "PL", "PT", "RO", "RU", "SK", "SL", "SV", "TR", "UK", "ZH"]
-    targets = ["BG", "CS", "DA", "DE", "EL", "EN-GB", "EN-US", "ES", "ET", "FI", "FR", "HU", "ID", "IT", "JA", "KO", "LT", "LV", "NB", "NL", "PL", "PT-BR", "PT-PT", "RO", "RU", "SK", "SL", "SV", "TR", "UK", "ZH"]
+    sources = ["BG", "CS", "DA", "DE", "EL", "EN", "ES", "ET", "FI", "FR", "HU", "ID", "IT", "JA",
+               "KO", "LT", "LV", "NB", "NL", "PL", "PT", "RO", "RU", "SK", "SL", "SV", "TR", "UK", "ZH"]
+    targets = ["BG", "CS", "DA", "DE", "EL", "EN-GB", "EN-US", "ES", "ET", "FI", "FR", "HU", "ID", "IT", "JA",
+               "KO", "LT", "LV", "NB", "NL", "PL", "PT-BR", "PT-PT", "RO", "RU", "SK", "SL", "SV", "TR", "UK", "ZH"]
     if source not in sources:
         return False
     if target not in targets:
         return False
-    result = translator.translate_text(text, source_lang=source, target_lang=target)
+    result = translator.translate_text(
+        text, source_lang=source, target_lang=target)
     if type(text) is list:
         return [r.text for r in result]
     else:
         return str(result.text)
 
 
-def translate_video(input_path,source_lang , target_lang, mode, GPU=False):
+def translate_video(input_path, source_lang, target_lang, mode, GPU=False):
 
     mode = int(mode)
     if mode == 0:
-        scale_factor = 1 # 60 fps -> 60 fps , retain all frames
+        scale_factor = 1  # 60 fps -> 60 fps , retain all frames
     elif mode == 1:
-        scale_factor = 0.1 # 60 fps -> 6 fps , retain 1 frame every 10 frames
+        scale_factor = 0.1  # 60 fps -> 6 fps , retain 1 frame every 10 frames
     elif mode == 2:
         scale_factor = 0.05  # 60 fps -> 3 fps , retain 1 frame every 20 frames
     elif mode == 3:
-        scale_factor = 0.01 # 60 fps -> 1 fps , retain 1 frame every 60 frames
+        scale_factor = 0.01  # 60 fps -> 1 fps , retain 1 frame every 60 frames
     else:
         print("Error: Invalid mode")
         exit()
-    
+
     start_time = 0
     skip_frames = int(1 / scale_factor)
     video_path = os.path.abspath(input_path)
@@ -47,7 +51,7 @@ def translate_video(input_path,source_lang , target_lang, mode, GPU=False):
     width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
     height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
     out = cv2.VideoWriter(out_path, fourcc, fps, (width, height))
-    reader = easyocr.Reader(['en','fr','es','de'], gpu=GPU)
+    reader = easyocr.Reader(['en', 'fr', 'es', 'de'], gpu=GPU)
 
     if not cap.isOpened():
         print("Error: Couldn't open the video file")
@@ -72,25 +76,28 @@ def translate_video(input_path,source_lang , target_lang, mode, GPU=False):
             previous_frame = frame.copy()
             continue
         result = reader.readtext(frame)
-        print(result)
+        # print(result)
         translated_frame = frame.copy()
         for detection in result:
             top_left = tuple(map(int, detection[0][0]))
             bottom_right = tuple(map(int, detection[0][2]))
-            bg_color = np.mean(frame[top_left[1]-1:top_left[1]+1, bottom_right[0]:bottom_right[0]+1], axis=(0, 1))
+            bg_color = np.mean(
+                frame[top_left[1]-1:top_left[1]+1, bottom_right[0]:bottom_right[0]+1], axis=(0, 1))
             bg_color = tuple(map(int, bg_color))
-            cv2.rectangle(translated_frame, top_left, bottom_right, bg_color, -1)
+            cv2.rectangle(translated_frame, top_left,
+                          bottom_right, bg_color, -1)
             pil_image = Image.fromarray(translated_frame)
             text_height = bottom_right[1] - top_left[1]
             font_size = int(text_height / 2)
             draw = ImageDraw.Draw(pil_image)
             text = detection[1]
-            if(text == ''):
+            if (text == ''):
                 continue
             else:
-                text1 = translate(str(text),source_lang,target_lang)
+                text1 = translate(str(text), source_lang, target_lang)
             font = ImageFont.truetype(font_path, font_size)
-            draw.text((top_left[0], top_left[1]), text1, font=font, fill=(255-bg_color[0], 255-bg_color[1], 255-bg_color[2]))
+            draw.text((top_left[0], top_left[1]), text1, font=font, fill=(
+                255-bg_color[0], 255-bg_color[1], 255-bg_color[2]))
             translated_frame = np.array(pil_image)
         out.write(translated_frame)
         previous_translated_frame = translated_frame.copy()
@@ -102,4 +109,5 @@ def translate_video(input_path,source_lang , target_lang, mode, GPU=False):
     out.release()
     cv2.destroyAllWindows()
 
-translate_video('french2.mp4', 'FR', 'EN-GB', 2,True)
+
+translate_video('french2.mp4', 'FR', 'EN-GB', 2, True)
