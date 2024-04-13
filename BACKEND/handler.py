753d45c8519c2c885322c0958c44da6ce9fc77e3
Ansh Mehta <anshkapilmehta@gmail.com>
2024-04-13 16:18:55 +0530

    good working stuff

--- BACKEND/handler.py
+++ BACKEND/handler.py
@@ -1,25 +1,61 @@
 import cv2
 import easyocr
 import numpy as np
+import os
+from PIL import Image, ImageDraw, ImageFont
+import deepl
 
-video_path = 'assets/hack.mp4'
+deepl_api_key = os.getenv('DEEPL_API_KEY')  
+translator = deepl.Translator('16f7a326-13d2-42df-b379-3c6b56c0acd7:fx')
+
+def translate(text, source, target):
+    sources = ["BG", "CS", "DA", "DE", "EL", "EN", "ES", "ET", "FI", "FR", "HU", "ID", "IT", "JA", "KO", "LT", "LV", "NB", "NL", "PL", "PT", "RO", "RU", "SK", "SL", "SV", "TR", "UK", "ZH"]
+    targets = ["BG", "CS", "DA", "DE", "EL", "EN-GB", "EN-US", "ES", "ET", "FI", "FR", "HU", "ID", "IT", "JA", "KO", "LT", "LV", "NB", "NL", "PL", "PT-BR", "PT-PT", "RO", "RU", "SK", "SL", "SV", "TR", "UK", "ZH"]
+
+    if source not in sources:
+        return False
+    if target not in targets:
+        return False
+    
+    #unsupported languages return false
+    
+    result = translator.translate_text(text, source_lang=source, target_lang=target)
+    if type(text) is list:
+        return [r.text for r in result]
+    else:
+        return str(result.text)
+
+
 start_time = 0
+skip_frames = 200  
 
-reader = easyocr.Reader(['en'], gpu=False)
+
+video_path = os.path.abspath('french.mp4')
+
+reader = easyocr.Reader(['en','fr'], gpu=True)
 
 cap = cv2.VideoCapture(video_path)
 
+print(cap.isOpened())
 if not cap.isOpened():
     print("Error: Couldn't open the video file")
     exit()
 
 cap.set(cv2.CAP_PROP_POS_MSEC, start_time)
+frame_count = 0  
+
 
 while True:
     ret, frame = cap.read()
 
     if not ret:
         break
+
+    if frame_count % skip_frames != 0:  
+        frame_count += 1
+        continue
+
+
 
     result = reader.readtext(frame)
     print(result)
@@ -42,6 +78,7 @@ while True:
 
         # placeholder for now, @manan dont forget to change before translation
         text = detection[1]
+        text1 = str(translate(text, 'EN', 'FR'))
 
         text_size = cv2.getTextSize(text, cv2.FONT_HERSHEY_SIMPLEX, 1, 2)[0]
         text_x = top_left[0] + \
@@ -52,13 +89,16 @@ while True:
         comp_bg_color = (255-bg_color[0], 255-bg_color[1], 255-bg_color[2])
         # print(comp_bg_color)
 
-        cv2.putText(frame, text, (text_x, text_y),
-                    cv2.FONT_HERSHEY_SIMPLEX, 1, comp_bg_color, 2)
+        cv2.putText(frame, text1, (text_x, text_y),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.8, comp_bg_color, 2)
 
     cv2.imshow('Frame', frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
+    frame_count += 1  # Increment frame counter
+
 
 cap.release()
 cv2.destroyAllWindows()
