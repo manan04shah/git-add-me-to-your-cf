82274c4a40efea1f42ee8df13ea58be9d160b529
Ansh Mehta <anshkapilmehta@gmail.com>
2024-04-14 16:37:25 +0530

    final

--- main.py
+++ main.py
@@ -5,11 +5,14 @@ from fastapi.middleware.cors import CORSMiddleware
 import os
 from BACKEND import handler
 from moviepy.editor import *
+import dropbox
+
 
 #Import functions directory
 import sys
 sys.path.append('BACKEND')
-# from BACKEND.audiotranslate.functions import createTranslatedAV as ctav
+
+DROPBOX_API_KEY = "sl.BzU1JJ1VcttI7jjlWeqQaURN41XmddmWwDTxzKHeCLhGpsXQ3g6c1XsQTKaT-2fx853Tk0DL5PHgAsWDJ25Omaa0mQY-LaIXWsXrNTCWrpUGX3ScklltZ4a5lp57faEaQUS8S0a941n4Fs0bPN57ZgM"
 
 app = FastAPI()
 
@@ -48,8 +51,12 @@ async def run_command(command: str):
 @app.post("/translateaudio/") #Translating ONLY the audio
 async def translate_audio(videofile: UploadFile = File(...), source_lang: str = "en", target_lang: str = "hi"):
     upload_dir = "BACKEND/audiotranslate/videos"
-    if not os.path.exists(upload_dir):
-        os.makedirs(upload_dir)
+    source_lang = source_lang.lower()
+    target_lang = target_lang.lower()
+    if(source_lang == "en-gb"):
+        source_lang = "en"
+    if(target_lang == "en-gb"):
+        target_lang = "en"
 
     file_location = os.path.join(upload_dir, videofile.filename)
     with open(file_location, "wb+") as file_object:
@@ -66,9 +73,19 @@ async def translate_audio(videofile: UploadFile = File(...), source_lang: str = 
 
     #Run the createAV function
     print(os.system(f"cd BACKEND/audiotranslate/functions && python3 createTranslatedAV.py {filepath} {source_lang} {target_lang}"))
-    
-    # Return the FileResponse for the translated video
-    return FileResponse(output_filepath, media_type="video/mp4", filename=videofile.filename)
+
+    dbx = dropbox.Dropbox(DROPBOX_API_KEY)
+    print(dbx.users_get_current_account())
+
+    with open(output_filepath, "rb") as f:
+        dbx.files_upload(f.read(), "/audio_output.mp4", mode=dropbox.files.WriteMode.overwrite)
+
+    #Obtain a shareable link of this uploaded file
+    shared_link = dbx.sharing_create_shared_link("/audio_output.mp4")
+
+    print(shared_link)
+
+    return ({"shareableLink": shared_link.url})
 
 @app.post("/translatevideo/") #Translating ONLY the video
 async def translate_video(source_lang: str, target_lang: str, mode: int, videofile: UploadFile = File(...)):
@@ -84,6 +101,19 @@ async def translate_video(source_lang: str, target_lang: str, mode: int, videofi
     handler.translate_video(file_location, source_lang, target_lang, mode, True)
 
     output_filepath = "output.mp4"
+
+    dbx = dropbox.Dropbox(DROPBOX_API_KEY)
+    print(dbx.users_get_current_account())
+
+    with open("output.mp4", "rb") as f:
+        dbx.files_upload(f.read(), "/output.mp4", mode=dropbox.files.WriteMode.overwrite)
+
+    #Obtain a shareable link of this uploaded file
+    shared_link = dbx.sharing_create_shared_link("/output.mp4")
+
+    print(shared_link)
+
+    return ({"shareableLink": shared_link.url})
 
     # Return the FileResponse for the translated video
     return FileResponse(output_filepath, media_type="video/mp4", filename=videofile.filename)
@@ -137,5 +167,15 @@ async def translate_av(source_lang: str, target_lang: str, mode: int, videofile:
 
     final_clip.write_videofile("final_output.mp4")
 
-    # Return the FileResponse for the translated video
-    return FileResponse("final_output.mp4", media_type="video/mp4", filename=videofile.filename)
+    dbx = dropbox.Dropbox(DROPBOX_API_KEY)
+    print(dbx.users_get_current_account())
+
+    with open("final_output.mp4", "rb") as f:
+        dbx.files_upload(f.read(), "/final_output.mp4", mode=dropbox.files.WriteMode.overwrite)
+
+    #Obtain a shareable link of this uploaded file
+    shared_link = dbx.sharing_create_shared_link("/final_output.mp4")
+
+    print(shared_link)
+
+    return ({"shareableLink": shared_link.url})
